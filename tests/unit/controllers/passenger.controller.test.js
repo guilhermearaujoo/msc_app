2ef6c2b9a1e6694812f3c6f79a96c1f742ec4ea5
Jonathan Ferreira <j0n4t@hotmail.com>
2022-09-24 17:27:51 -0300

    feat(controller): `passenger`: testes

--- tests/unit/controllers/passenger.controller.test.js
+++ tests/unit/controllers/passenger.controller.test.js
@@ -11,6 +11,8 @@ const {
   happyControllerResponseCreateTravelObject,
   happyReqCreateTravelObject,
   happyResponseCreateTravelObject,
+  passengerMock,
+  newPassengerMock,
 } = require('./mocks/passenger.controller.mock');
 
 describe('Teste de unidade do passengerController', function () {
@@ -28,6 +30,59 @@ describe('Teste de unidade do passengerController', function () {
 
     expect(res.status).to.have.been.calledWith(201);
     expect(res.json).to.have.been.calledWith(happyResponseCreateTravelObject);
+  });
+
+  it('Listando as pessoas passageiras', async function () {
+    const res = {};
+    const req = {};
+    const passengerList = [newPassengerMock];
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+    sinon
+      .stub(passengerService, 'findAll')
+      .resolves({ type: null, message: passengerList });
+
+    await passengerController.listPassengers(req, res);
+
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWith(passengerList);
+  });
+
+  it('Buscando uma pessoa passageira', async function () {
+    const res = {};
+    const req = {
+      params: { id: 1 },
+    };
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+    sinon
+      .stub(passengerService, 'findById')
+      .resolves({ type: null, message: newPassengerMock });
+
+    await passengerController.getPassenger(req, res);
+
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWith(newPassengerMock);
+  });
+
+  it('Cadastrando uma nova pessoa passageira com sucesso', async function () {
+    const res = {};
+    const req = {
+      body: passengerMock,
+    };
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+    sinon
+      .stub(passengerService, 'createPassenger')
+      .resolves({ type: null, message: newPassengerMock });
+
+    await passengerController.createPassenger(req, res);
+
+    expect(res.status).to.have.been.calledWith(201);
+    expect(res.json).to.have.been.calledWith(newPassengerMock);
   });
 
   afterEach(sinon.restore);

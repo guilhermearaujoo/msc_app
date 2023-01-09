077a99d271194eb7a4734a1e4278bc1c592ff88b
Renato Filho <renatosousafilho@gmail.com>
2023-01-09 17:22:39 -0300

    3.1 Adcicionando o model `drivers`

--- src/models/index.js
+++ src/models/index.js
@@ -1,9 +1,11 @@
-const passengerModel = require('./passenger.model');
 const travelModel = require('./travel.model');
+const passengerModel = require('./passenger.model');
 const waypointModel = require('./waypoint.model');
+const driverModel = require('./driver.model');
 
 module.exports = {
-  passengerModel,
   travelModel,
+  passengerModel,
   waypointModel,
+  driverModel,
 };

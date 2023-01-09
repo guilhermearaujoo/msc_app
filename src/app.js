5273554a73d31c5f29136d8a0cd054bef3f02cbb
Renato Filho <renatosousafilho@gmail.com>
2023-01-09 19:25:55 -0300

    5.7 - Refatorando rotas /drivers/*

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const { passengerRouter } = require('./routers');
-/* Removemos a importação do passengerService pois ele não é mais usado aqui! */
-const { driverService } = require('./services');
+/* Removemos a importação do driverService pois ele não é mais usado aqui
+e importamos o objeto driverRoutes! */
+const { passengerRouter, driverRoutes } = require('./routers');
 
 const app = express();
 
@@ -10,35 +10,8 @@ app.use(express.json());
 /* Adicionamos o registro das rotas para o CRUD de pessoas passageiras */
 app.use('/passengers', passengerRouter);
 
-/* Apagamos o código que ficava aqui! */
-
-app.get('/drivers/open/travels', async (_req, res) => {
-  const result = await driverService.getWaitingDriverTravels();
-  res.status(200).json(result);
-});
-
-app.put('/drivers/:driverId/travels/:travelId/assign', async (req, res) => {
-  const { travelId, driverId } = req.params;
-  const { type, message } = await driverService.travelAssign({ travelId, driverId });
-  if (type) return res.status(type).json(message);
-  
-  res.status(200).json(message);
-});
-
-app.put('/drivers/:driverId/travels/:travelId/start', async (req, res) => {
-  const { travelId, driverId } = req.params;
-  const { type, message } = await driverService.startTravel({ travelId, driverId });
-  if (type) return res.status(type).json(message);
-
-  res.status(200).json(message);
-});
-
-app.put('/drivers/:driverId/travels/:travelId/end', async (req, res) => {
-  const { travelId, driverId } = req.params;
-  const { type, message } = await driverService.endTravel({ travelId, driverId });
-  if (type) return res.status(type).json(message);
-
-  res.status(200).json(message);
-});
+/* Aqui apagamos todos o código que movemos para o do router de pessoas motoristas
+e associamos o mesmo para ser responsável por qualquer rota que começar com /drivers */
+app.use('/drivers', driverRoutes);
 
 module.exports = app;

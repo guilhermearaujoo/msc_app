c546a62d96a057d4b3eddc998005fc5488f23deb
Renato Filho <renatosousafilho@gmail.com>
2023-01-10 10:25:05 -0300

    5.7.1 - Envelopando teste de getWaitingDriverTravels dentro de um describe

--- tests/unit/controllers/driver.controller.test.js
+++ tests/unit/controllers/driver.controller.test.js
@@ -11,35 +11,37 @@ const { driverService } = require('../../../src/services');
 const { driverController } = require('../../../src/controllers');
 
 describe('Teste de unidade do driverController', function () {
-  it('Buscando as viagens em aberto quando não tem nenhuma viagem cadastrada', async function () {
-    // Este é o objeto de resposta (res) inicialmente é um objeto vazio
-    // que será preenchido pelo express.
-    const res = {};
+  describe('Buscando as viagens em aberto', function () {
+    it('quando não tem nenhuma viagem cadastrada retorna um array vazio', async function () {
+      // Este é o objeto de resposta (res) inicialmente é um objeto vazio
+      // que será preenchido pelo express.
+      const res = {};
 
-    // Este é o objeto de requisição (req) que contém os dados necessários
-    // para a requisição. Como a requisição é um GET não é esperado nenhum
-    // dado durante a requisição.
-    const req = {};
+      // Este é o objeto de requisição (req) que contém os dados necessários
+      // para a requisição. Como a requisição é um GET não é esperado nenhum
+      // dado durante a requisição.
+      const req = {};
 
-    // Criamos um stub para a função "res.status" que retorna o objeto res quando executada
-    res.status = sinon.stub().returns(res);
+      // Criamos um stub para a função "res.status" que retorna o objeto res quando executada
+      res.status = sinon.stub().returns(res);
 
-    // Criamos um stub para a função "res.json" que não retorna nada
-    res.json = sinon.stub().returns();
+      // Criamos um stub para a função "res.json" que não retorna nada
+      res.json = sinon.stub().returns();
 
-    // Criamos um stub para a chamada do service "driverService.getWaitingDriverTravels" que irá
-    // retornar uma resposta com um array vazio
-    sinon
-      .stub(driverService, 'getWaitingDriverTravels')
-      .resolves({ type: null, message: [] });
+      // Criamos um stub para a chamada do service "driverService.getWaitingDriverTravels" que irá
+      // retornar uma resposta com um array vazio
+      sinon
+        .stub(driverService, 'getWaitingDriverTravels')
+        .resolves({ type: null, message: [] });
 
-    // Realizamos a chamada para o controller simulando o recebimento de uma requisição
-    await driverController.openTravel(req, res);
+      // Realizamos a chamada para o controller simulando o recebimento de uma requisição
+      await driverController.openTravel(req, res);
 
-    // Validamos se o status code da resposta é igual a 200
-    expect(res.status).to.have.been.calledWith(200);
+      // Validamos se o status code da resposta é igual a 200
+      expect(res.status).to.have.been.calledWith(200);
 
-    // Validamos se o json da resposta é igual a um array vazio
-    expect(res.json).to.have.been.calledWith([]);
+      // Validamos se o json da resposta é igual a um array vazio
+      expect(res.json).to.have.been.calledWith([]);
+    });
   });
 });
